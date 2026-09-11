4224088855310a2a93893e1c799362fe705a6d37
CKeat0906 <chunkeat990906@gmail.com>
2026-09-11 17:03:21 +0800

    Update File_Json.py

--- File_Json.py
+++ File_Json.py
@@ -1,69 +1,198 @@
-import json
+class Product:
+    def __init__(self,name,price,stock):
+        self.name = name
+        self.__price = price
+        self.__stock = stock
 
-products = [
-    {"name": "Apple", "price": 3.5, "stock": 20},
-    {"name": "Orange", "price": 5, "stock": 15}
-]
+    def __str__(self):
+        return (f"Name: {self.name}\n"
+                f"   Price : RM{self.__price:.2f}\n"
+                f"   Stock: {self.__stock}")
 
-# ==============================
-# dump() / load() (操作 JSON 文件)
-# ==============================
+    def to_dict(self):
+        return {"name": self.name,
+                "price": self.get_price(),
+                "stock": self.get_stock()}
 
-# json.dump()
-# 将 Python 资料 (list、dict...) 储存到 JSON 文件
+    def change_price(self,price):
+        if price <= 0:
+            print("Price Must Greater Than 0")
+            return False
+        else:
+            self.__price = price
+            print(f"Price {self.name} Amend to RM{self.__price:.2f} Success!")
+            return True
 
-# json.load()
-# 从 JSON 文件读取资料，并转换回 Python 的 list 或 dict
+    def add_stock(self,qty):
+        if qty < 1:
+            print("Prompt Error")
+            return False
+        else:
+            self.__stock += qty
+            print(f"Stock {self.name} add in stock quantity {qty} success")
+            return True
 
-# encoding="utf-8"
-# 告诉 Python 使用 UTF-8 编码来储存或读取文件，避免乱码
+    def remove_stock(self,stock):
+        if stock > self.__stock:
+            return False
+        else:
+            self.__stock -= stock
+            return True
 
-# indent=4
-# 让 JSON 文件排版更整齐，方便阅读
+    def sell(self,sell):
+        if self.remove_stock(sell):
+            print(f"{self.name} sell {sell} Success")
+        else:
+            print("Stock Not Enough")
 
-# ensure_ascii=False
-# 中文、日文等字符直接保存，不转换成 \uXXXX
+    def get_price(self):
+        return self.__price
 
-# .json 不是必须，但建议保留
-# 方便辨识这是 JSON 文件
+    def get_stock(self):
+        return self.__stock
 
-with open("products.json", "w", encoding="utf-8") as file:
-    json.dump(products, file, indent=4, ensure_ascii=False)
+class Inventory:
+    def __init__(self):
+        self.products = []
 
-with open("products.json", "r", encoding="utf-8") as file:
-    products = json.load(file)
+    def to_list(self):
+        products_list = []
+        for product in self.products:
+            item = product.to_dict()
+            products_list.append(item)
 
-for product in products:
-    print(f"Product : {product['name']}")
-    print(f"Price   : RM {product['price']:.2f}")
-    print()
+        return products_list
 
-# ==========================================
-# dumps() / loads() (操作 JSON 字符串，不是文件)
-# ==========================================
+    def add_product(self,product):
+        self.products.append(product)
 
-# json.dumps()
-# 将 Python 物件 (list、dict...) 转换成 JSON 字符串 (str)
+    def view_products(self):
+        for index, product in enumerate(self.products):
+            print(f"{index+1}. {product}\n\n")
 
-text = json.dumps(products)
+    def search_product(self,item):
+        for product in self.products:
+            if item.title().strip() == product.name:
+                print(product)
+                return True
 
-print(text)
-print(type(text))      # <class 'str'>
+        print("Product Not Found")
+        return False
 
-# json.loads()
-# 将 JSON 字符串 (str) 转换回 Python 的 list 或 dict
+    def delete_product(self,item):
+        for index,product in enumerate(self.products):
+            if item.title().strip() == product.name:
+                self.products.pop(index)
+                print(f"{item} Delete Success")
+                return True
 
-data = json.loads(text)
+        print("Product Not Found")
+        return False
 
-print(data)
-print(type(data))      # <class 'list'>
+    def edit_product_price(self,name,price):
+        name = name.title().strip()
+        for product in self.products:
+            if name == product.name:
+                if product.change_price(price):
+                    return True
+                else:
+                    return False
 
-# 容易记的方式
-# dump   -> File
-# load   -> File
+        print("Product Not Found")
+        return False
 
-# dumps  -> String
-# loads  -> String
+    def add_stock2 (self,name,stock):
+        name = name.title().strip()
+        for product in self.products:
+            if name == product.name:
+                return product.add_stock(stock)
 
-# 没有 s = File（文件）
-# 有 s = String（字符串）
+        print("Product Not Found")
+        return False
+
+    def remove_stock(self,name,stock):
+        name = name.title().strip()
+        for product in self.products:
+            if name == product.name:
+                if product.remove_stock(stock):
+                    print("Remove Success")
+                    return True
+                else:
+                    print("Not Enough Stock")
+                    return False
+        print("Product Not Found")
+        return False
+
+def error_int(prompt):
+    while True:
+        try:
+            value = int(input(prompt))
+
+            if value <= 0:
+                print("Quantity Cant Be Negative")
+            else:
+                return value
+
+        except ValueError:
+            print("Error")
+
+def error_float(prompt):
+    while True:
+        try:
+            return float(input(prompt))
+        except ValueError:
+            print("Error")
+
+
+
+
+inventory = Inventory()
+
+while True:
+    choice = error_int("===== Inventory Management System =====\n"
+                        "1. Add Product\n"
+                        "2. View Product\n"
+                        "3. Search Product\n"
+                        "4. Delete Product\n"
+                        "5. Edit Price\n"
+                        "6. Add Stock\n"
+                        "7. Remove Stock\n"
+                        "8. Exit\n"
+                        "Enter Your Choice: ")
+    if choice == 1:
+        name = input("Product Name: ").title().strip()
+        price = error_float("Product price: RM")
+        stock = error_int("Stock Quantity: ")
+        product = Product(name,price,stock)
+        inventory.add_product(product)
+
+    elif choice == 2:
+        inventory.view_products()
+
+    elif choice == 3:
+        search = input("Key In Product You Search: ")
+        inventory.search_product(search)
+
+    elif choice == 4:
+        inventory.view_products()
+        delete = input("Key In Product You Want Remove: ")
+        inventory.delete_product(delete)
+
+    elif choice == 5:
+        name = input("Product Name: ")
+        price = error_float("Product Price: RM")
+        inventory.edit_product_price(name,price)
+
+    elif choice == 6:
+        name = input("Product Name: ")
+        stock = error_int("Stock Qty Add: ")
+        inventory.add_stock2(name,stock)
+
+    elif choice == 7:
+        name = input("Product Name: ")
+        stock = error_int("Stock Qty Remove: ")
+        inventory.remove_stock(name, stock)
+
+    elif choice == 8:
+        print("Goodbye")
+        break
